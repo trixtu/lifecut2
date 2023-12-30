958aeee4a4d34f42b0d35f055185261f15b37b22
trixtu <trixtu12@gmail.com>
2023-12-30 09:27:09 +0100

    update

--- components/slider/SlideNavButtons.js
+++ components/slider/SlideNavButtons.js
@@ -1,15 +1,71 @@
 import { useSwiper } from 'swiper/react'
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa6'
+import { useEffect, useRef } from 'react'
+import gsap from 'gsap'
 
-const SlideNavButtons = ({hover}) => {
+const SlideNavButtons = ({ hover, handleMouseEnter }) => {
     const swiper = useSwiper()
+    const buttonsRefLeft = useRef(null)
+    const buttonsRefRight = useRef(null)
+
+    useEffect(() => {
+        if (hover) {
+            gsap.to(buttonsRefLeft.current, {
+                x: 30,
+                duration: 1,
+                opacity: 2,
+                duration: 0.5,
+                ease: 'power2.inOut',
+            })
+            gsap.to(buttonsRefRight.current, {
+                x: -30,
+                duration: 1,
+                opacity: 2,
+                duration: 0.5,
+                ease: 'power2.inOut',
+            })
+        } else {
+            gsap.to(buttonsRefLeft.current, {
+                x: 0,
+                duration: 0.5,
+                opacity: 0,
+                duration: 0.5,
+                ease: 'power2.inOut',
+            })
+            gsap.to(buttonsRefRight.current, {
+                x: 0,
+                duration: 0.5,
+                opacity: 0,
+                duration: 0.5,
+                ease: 'power2.inOut',
+            })
+        }
+    }, [hover])
 
     return (
         <div>
-            <button className={hover ? 'absolute z-40 top-[48%] bg-slate-50 p-2 ml-4 shadow-sm border' : 'hidden'} onClick={() => swiper.slidePrev()} >
+            <button
+                className={
+                    hover
+                        ? 'absolute z-40 top-[48%] bg-slate-50 p-3 shadow-lg border'
+                        : 'hidden'
+                }
+                onClick={() => swiper.slidePrev()}
+                onMouseEnter={handleMouseEnter}
+                ref={buttonsRefLeft}
+            >
                 <FaChevronLeft />
             </button>
-            <button className={hover ? 'absolute z-40 top-[48%] bg-slate-50 p-2 mr-4 shadow-sm border right-0' : 'hidden'} onClick={() => swiper.slideNext()}>
+            <button
+                className={
+                    hover
+                        ? 'absolute z-40 top-[48%] bg-slate-50 p-3 shadow-lg border right-0'
+                        : 'hidden'
+                }
+                onClick={() => swiper.slideNext()}
+                onMouseEnter={handleMouseEnter}
+                ref={buttonsRefRight}
+            >
                 <FaChevronRight />
             </button>
         </div>
--- components/slider/SliderHomePage.js
+++ components/slider/SliderHomePage.js
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import React from 'react'
 import { Autoplay, Navigation, Pagination, A11y } from 'swiper/modules'
 import { Swiper, SwiperSlide } from 'swiper/react'
@@ -7,32 +7,39 @@ import SlideNavButtons from './SlideNavButtons'
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
+import gsap from 'gsap'
 
 const slideImages = [
     {
-        url: 'https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+        url: 'https://www.hoerner-gmbh.com/wp-content/uploads/2019/09/terra_3-e1699091652815.jpg',
         caption: 'Slide 1',
     },
     {
-        url: 'https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80',
+        url: 'https://www.hoerner-gmbh.com/wp-content/uploads/2023/02/Slider-Werbemittel_CTA.jpg',
         caption: 'Slide 2',
     },
     {
-        url: 'https://images.unsplash.com/photo-1536987333706-fc9adfb10d91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80',
+        url: 'https://www.hoerner-gmbh.com/wp-content/uploads/2023/03/B150_Wit_Verlichting-2019-8-scaled-e1699090848755.jpg',
         caption: 'Slide 3',
+    },
+    {
+        url: 'https://www.hoerner-gmbh.com/wp-content/uploads/2019/09/markisen_beschattungen-e1699091258617.jpg',
+        caption: 'Slide 4',
     },
 ]
 
 const SliderHomePage = () => {
     const [hover, setHover] = useState(false)
     let hoverTimeout
-
+   
     const progressCircle = useRef(null)
     const progressContent = useRef(null)
     const onAutoplayTimeLeft = (s, time, progress) => {
         progressCircle.current.style.setProperty('--progress', 1 - progress)
         progressContent.current.textContent = `${Math.ceil(time / 1000)}s`
     }
+
+    
 
     const handleMouseEnter = () => {
         clearTimeout(hoverTimeout)
@@ -43,42 +50,50 @@ const SliderHomePage = () => {
         // Setează un timeout pentru a reseta starea de hover după 10 secunde
         hoverTimeout = setTimeout(() => {
             setHover(false)
-        }, 1500)
+        }, 500)
     }
     return (
-        <div>
-            <Swiper
-                spaceBetween={50}
-                slidesPerView={1}
-                pagination={true}
-                autoplay={{
-                    delay: 5000,
-                    disableOnInteraction: false,
-                }}
-                modules={[Autoplay, Navigation, Pagination]}
-                onAutoplayTimeLeft={onAutoplayTimeLeft}
-                className="mySwiper relative"
-            >
-                {slideImages.map((image, index) => (
-                    <SwiperSlide key={index}>
+        <Swiper
+            rewind={true}
+            spaceBetween={50}
+            slidesPerView={1}
+            pagination={true}
+            autoplay={{
+                delay: 5000,
+                disableOnInteraction: false,
+            }}
+            modules={[Autoplay, Navigation, Pagination]}
+            onAutoplayTimeLeft={onAutoplayTimeLeft}
+            className="mySwiper relative"
+            ref={sliderRef}
+        >
+            {slideImages.map((image, index) => (
+                <SwiperSlide key={index}>
+                    <div className="slider-image-container">
                         <img
-                            className="max-h-[280px] md:max-h-[400px] lg:max-h-[640px]"
+                            className=" max-h-[280px] md:max-h-[400px] lg:max-h-[640px]"
                             onMouseEnter={handleMouseEnter}
                             onMouseLeave={handleMouseLeave}
                             src={image.url}
                             alt={image.caption}
                         />
-                    </SwiperSlide>
-                ))}
-                <SlideNavButtons setHover={setHover} hover={hover} />
-                <div className="autoplay-progress" slot="container-end">
-                    <svg viewBox="0 0 48 48" ref={progressCircle}>
-                        <circle cx="24" cy="24" r="20"></circle>
-                    </svg>
-                    <span ref={progressContent}></span>
-                </div>
-            </Swiper>
-        </div>
+                    </div>
+                </SwiperSlide>
+            ))}
+            <div className="slide-nav-buttons">
+                <SlideNavButtons
+                    hover={hover}
+                    handleMouseEnter={handleMouseEnter}
+                    buttonsRef={buttonsRef}
+                />
+            </div>
+            <div className="autoplay-progress" slot="container-end">
+                <svg viewBox="0 0 48 48" ref={progressCircle}>
+                    <circle cx="24" cy="24" r="20"></circle>
+                </svg>
+                <span ref={progressContent}></span>
+            </div>
+        </Swiper>
     )
 }
 
